62bfe9a60cf12f3811086099f9ddd901ed4467a4
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-02-07 22:44:24 +0200

    clear console, fix styles

--- src/js/trending-search-main/fetch-genres.js
+++ src/js/trending-search-main/fetch-genres.js
@@ -1,26 +1,24 @@
 import API_KEY from './api-key';
-import BASE_URL from './base-url';
 
 async function getGenres() {
-  const data = await fetch(
-    `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
-  );
-  // отримуємо назви жанрів
-  const genresData = await data.json();
-  const { genres } = genresData;
-  console.log(genres);
+  try {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
+    );
+    // отримуємо назви жанрів
+    const genresData = await data.json();
+    const { genres } = genresData;
 
-  // масив об'єктів з властивістю id i значенням name
+    // масив об'єктів з властивістю id i значенням name
+    const genreNames = {};
+    genres.map(item => {
+      genreNames[item.id] = item.name;
+    });
 
-  const genreNames = {};
-
-  genres.map(item => {
-    genreNames[item.id] = item.name;
-  });
-
-  console.log('genreNames', genreNames);
-
-  return genreNames;
+    return genreNames;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default getGenres;
--- src/js/trending-search-main/trending-search.js
+++ src/js/trending-search-main/trending-search.js
@@ -1,5 +1,3 @@
-import API_KEY from './api-key';
-import BASE_URL from './base-url';
 import fetchPopularFilms from './fetch-movies';
 import getImages from './fetch-images-url';
 import getGenres from './fetch-genres';
@@ -7,63 +5,51 @@ import getGenres from './fetch-genres';
 const cardList = document.querySelector('.trending-gallery');
 
 async function createMarkup(data) {
-  console.log('data results', data);
-  const imageBaseURL = await getImages();
+  try {
+    const imageBaseURL = await getImages();
+    const genreNames = await getGenres();
 
-  const genreNames = await getGenres();
-  console.log('genreNames in markup', genreNames);
-
-  // const imageBaseURL = 'https://image.tmdb.org/t/p/original';
-  console.log(imageBaseURL);
-
-  const markup = data
-    .map(item => {
-      let genres = '';
-      const genresNamesToRender = getGenreDeciphered(item, genreNames);
-      if (genresNamesToRender.length > 2) {
-        genres = `${genresNamesToRender[0]}, ${genresNamesToRender[1]}, Other`;
-      } else {
-        genres = genresNamesToRender;
-      }
-
-      console.log('item', item);
-      return `<li class="trending-gallery__item">
+    const markup = data
+      .map(item => {
+        let genres = '';
+        const genresNamesToRender = getGenreDeciphered(item, genreNames);
+        if (genresNamesToRender.length > 2) {
+          genres = `${genresNamesToRender[0]}, ${genresNamesToRender[1]}, Other`;
+        } else {
+          genres = `${genresNamesToRender[0]}, ${genresNamesToRender[1]}`;
+        }
+        return `<li class="trending-gallery__item">
       <img src="${imageBaseURL}${item.poster_path}" 
             alt="The poster of ${item.title} film" 
             class="trending-gallery__image" />
       <div class="trending-gallery__wrapper">
       <h3 class="trending-gallery__title">${item.title}</h3>
       <p class="trending-gallery__info">${genres} | <span>${item.release_date.slice(
-        0,
-        4
-      )}</span></p>
+          0,
+          4
+        )}</span></p>
       </div>
       </li>`;
-    })
-    .join('');
-  return markup;
+      })
+      .join('');
+    return markup;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function getGenreDeciphered(filmObject, genresList) {
   const genreArray = filmObject.genre_ids;
-
   const genreNamesToRender = genreArray.map(item => {
     return genresList[item];
   });
-
-  console.log('genresToRender', genreNamesToRender);
   return genreNamesToRender;
 }
 
 async function renderPopularFilms() {
   try {
     const films = await fetchPopularFilms();
-    console.log(films);
     const markup = await createMarkup(films);
-    console.log(films);
-    console.log(films.genre_ids);
-    // console.log(markup);
-
     cardList.innerHTML = markup;
   } catch (error) {
     console.dir(error);
