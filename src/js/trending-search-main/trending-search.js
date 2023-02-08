a243965fe0f72d7f8f87e264110ac8b321f70daf
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-02-09 00:39:18 +0200

    add onerror attribute to img

--- src/js/trending-search-main/trending-search.js
+++ src/js/trending-search-main/trending-search.js
@@ -29,7 +29,9 @@ async function createMarkup(data) {
         return `<li class="trending-gallery__item">
       <img src="${imageBaseURL}${item.poster_path}" 
             alt="The poster of ${item.title} film" 
-            class="trending-gallery__image" />
+            class="trending-gallery__image"
+            onerror="this.onerror=null;this.src='https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80';"
+            />
       <div class="trending-gallery__wrapper">
       <h3 class="trending-gallery__title">${item.title}</h3>
       <p class="trending-gallery__info">${genres}<span>${item.release_date.slice(
