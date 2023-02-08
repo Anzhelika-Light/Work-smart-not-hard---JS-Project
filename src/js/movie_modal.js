b91ffa4720e0f880f9d7b87aac597b683c04de06
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2023-02-08 12:13:31 +0200

    1st commit

---
+++ src/js/movie_modal.js
@@ -0,0 +1,56 @@
+import TmdbAPI from '../scripts/TMDB_API';
+
+const tmdbAPI = new TmdbAPI();
+
+const movieModalEl = document.querySelector('.modal');
+const galleryEl = document.querySelector('.gallery');
+
+function fetchFilmsByID(id) {
+  tmdbAPI.fetchFilmsByID(id)
+  .then(console.log);
+}
+
+function createMovieDetails(movieInfo) {
+  const movieDetails = movieInfo.map(el => {
+    return `
+    <div>
+      <img> 
+    <div/>
+    <div>
+      <h2>Hello<h2/>
+      <p><p/>
+      <p><p/>
+      <p><p/>
+      <p><p/>
+      <h3><h3/>
+      <p><p/>
+      <div>
+        <button><button/>
+        <button><button/>
+      <div/>
+    <div/>
+    `;
+  });
+
+  return movieDetails.join('');
+}
+
+async function onMovieClick(e) {
+  e.preventDefault();
+  // TmdbAPI.query = e.target.elements.searchQuery.value.trim();
+  TmdbAPI.page = 1;
+  console.log(e.target);
+  fetchFilmsByID(e.target.dataset.id);
+  // if (e.target.nodeName !== '.link')
+  //   try {
+  //     const data = await TmdbAPI.fetchFilmsByQuery();
+  //     console.log(data);
+
+  //     // movieModalEl.innerHTML = createMovieDetails(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+}
+
+
+galleryEl.addEventListener('click', onMovieClick);
