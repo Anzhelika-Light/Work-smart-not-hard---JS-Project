63f845c4e8a08d046859975c579e77d02aa39186
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-02-09 02:46:39 +0200

    add dark mode

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {
 import './js/pagination/setPaginationSettings';
 import renderPopularFilms from './js/trending-search-main/trending-search';
 import './js/trending-search-main/trending-search';
+import './js/dark-mode';
 
 export async function onLoadAnotherPage(e) {
   try {
@@ -19,3 +20,26 @@ export async function onLoadAnotherPage(e) {
 }
 import './js/movie_search';
 import { studentCards } from './js/footer-modal';
+
+// import Darkmode from 'darkmode-js';
+// //https://darkmodejs.learn.uno
+
+// // const options = {
+// //   bottom: '64px', // default: '32px'
+// //   right: 'unset', // default: '32px'
+// //   left: '32px', // default: 'unset'
+// //   time: '0.5s', // default: '0.3s'
+// //   mixColor: '#fff', // default: '#fff'
+// //   backgroundColor: '#fff', // default: '#fff'
+// //   buttonColorDark: '#100f2c', // default: '#100f2c'
+// //   buttonColorLight: '#fff', // default: '#fff'
+// //   saveInCookies: false, // default: true,
+// //   label: '🌓', // default: ''
+// //   autoMatchOsTheme: true, // default: true
+// // };
+
+// const options = {
+//   autoMatchOsTheme: false,
+// };
+// const darkmode = new Darkmode(options);
+// darkmode.showWidget();
