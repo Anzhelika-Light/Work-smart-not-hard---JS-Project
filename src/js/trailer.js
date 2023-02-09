8a39ce4958fdf511dd06a89a75055b92348fbc5b
Uliana Shymanska <yliana.shymanska1999@gmail.com>
2023-02-09 16:06:32 +0200

    Added feature/trailer

---
+++ src/js/trailer.js
@@ -0,0 +1,80 @@
+import TmdbAPI from './TMDB_API';
+import * as basicLightbox from 'basiclightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { spinnerStart, spinnerStop } from "./loader";
+
+const tmdbApi = new TmdbAPI();
+
+function fetchMovieTrailers(movieId) {
+  try {
+    const response = tmdbApi.fetchTrailersByID(movieId);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function onTrailerBtnClick(e) {
+  // spinnerStart()
+  const result = await fetchMovieTrailers(e.target.dataset.id);
+  if (!result.data.results.length) {
+    Notify.failure('Trailer not found!');
+  } else {
+    // spinnerStop()
+    createVideoFrame(result.data.results[0].key);
+  }
+}
+
+function createVideoFrame(key) {
+  const instance = basicLightbox.create(
+    `<div class="trailer-modal">
+    <iframe
+      class="trailer-modal__video"
+      width="100%"
+      height="100%"
+      src="https://www.youtube.com/embed/${key}?autoplay=1"
+      title="TITLE"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+</div>`,
+    {
+      onShow: instance => {
+        document.addEventListener('keydown', onEscClose);
+      },
+    },
+    {
+      onClose: instance => {
+        document.removeEventListener('keydown', onEscClose);
+        console.log('onCLose');
+      },
+    }
+  );
+
+  function onEscClose(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+  instance.show();
+  modalClBtTrailer(instance);
+}
+
+function modalClBtTrailer(instance) {
+  const modalBox = document.querySelector('.basicLightbox__placeholder');
+  modalBox.insertAdjacentHTML(
+    'afterbegin',
+    `<button
+        type="button"
+        class="lightbox__button"
+        data-action="close-lightbox"           
+        >
+      </button>
+    `
+  );
+  const modalCloseBtn = document.querySelector(
+    '[data-action="close-lightbox"]'
+  );
+  modalCloseBtn.addEventListener('click', () => instance.close());
+}
