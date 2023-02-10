07dafcdeeadd21968815f5cf4f7e82031519d7d2
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-02-10 20:09:07 +0200

    added dark mode to library page

--- src/library.js
+++ src/library.js
@@ -1,4 +1,5 @@
 import './js/my-library-watched-queue/my-library-watched-queue';
+import './js/dark-mode';
 import './js/footer-modal';
 import { spinnerStart, spinnerStop } from './js/loader';
 spinnerStart();
