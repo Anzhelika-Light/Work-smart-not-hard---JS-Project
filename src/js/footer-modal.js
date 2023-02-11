1aa5a58b3066dd03a21e30c2ab9fc05202161524
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-02-11 11:12:38 +0200

    fix bugs

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -31,7 +31,7 @@ const studentCards = basicLightbox.create(
           <h3 class="team-list__title">Svitlana Honchar</h3>
           <p class="team-list__text">
             <span class="team-list__position">Scrum-master,</span>
-            "Add to Watched", "Add to Queue" buttons, Code Review, Presentation
+            "Add to Watched", "Add to Queue" buttons, Presentation
           </p>
           <a href="https://github.com/SvitlanaHonchar" target="_blank" class="github"></a>
         </div>
